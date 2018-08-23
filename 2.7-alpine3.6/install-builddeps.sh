d4e0e235975819e5b3d2f5077566100810631e8e
Kishin Yagami <k.yagami.suou@gmail.com>
2018-08-23 22:49:24 +0900

    Update pkg

--- 2.7-alpine3.6/install-builddeps.sh
+++ 2.7-alpine3.6/install-builddeps.sh
@@ -10,7 +10,7 @@ GLIBC_SSH_KEY="/etc/apk/keys/sgerrand.rsa.pub"
 GLIBC_PKG_FILE="glibc-$GLIBC_PKG_VERSION.apk"
 GLIBC_BIN_PKG_FILE="glibc-bin-$GLIBC_PKG_VERSION.apk"
 apk add --no-cache --virtual=.fetchdeps wget ca-certificates
-wget -q -O "$GLIBC_SSH_KEY" "https://raw.githubusercontent.com/andyshinn/alpine-pkg-glibc/master/sgerrand.rsa.pub"
+wget -q -O "$GLIBC_SSH_KEY" "https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub"
 wget -q "$GLIBC_URL/$GLIBC_PKG_FILE" "$GLIBC_URL/$GLIBC_BIN_PKG_FILE"
 apk add --no-cache "$GLIBC_PKG_FILE" "$GLIBC_BIN_PKG_FILE"
 apk del .fetchdeps
@@ -23,17 +23,19 @@ apk add --no-cache \
   build-base="${BUILD_BASE_PKG_VERSION:-0.5-r0}" \
   bzip2-dev="${BZIP2_DEV_PKG_VERSION:-1.0.6-r5}" \
   bzip2="${BZIP2_PKG_VERSION:-1.0.6-r5}" \
-  curl="${CURL_PKG_VERSION:-7.59.0-r0}" \
-  git="${GIT_PKG_VERSION:-2.13.5-r0}" \
+  curl="${CURL_PKG_VERSION:-7.61.0-r0}" \
+  git="${GIT_PKG_VERSION:-2.13.7-r0}" \
   libffi-dev="${LIBFFI_DEV_PKG_VERSION:-3.2.1-r3}" \
   linux-headers="${LINUX_HEADERS_PKG_VERSION:-4.4.6-r2}" \
-  mercurial="${MERCURIAL_PKG_VERSION:-4.3.1-r0}" \
-  ncurses-dev="${NCURSES_DEV_PKG_VERSION:-6.0_p20171125-r0}" \
-  openssl-dev="${OPENSSL_DEV_PKG_VERSION:-1.0.2n-r0}" \
-  openssl="${OPENSSL_PKG_VERSION:-1.0.2n-r0}" \
+  mercurial="${MERCURIAL_PKG_VERSION:-4.5.2-r0}" \
+  ncurses-dev="${NCURSES_DEV_PKG_VERSION:-6.0_p20171125-r1}" \
+  openssl-dev="${OPENSSL_DEV_PKG_VERSION:-1.0.2o-r1}" \
+  openssl="${OPENSSL_PKG_VERSION:-1.0.2o-r1}" \
   patch="${PATCH_PKG_VERSION:-2.7.5-r3}" \
+  python2="2.7.15-r0" \
+  python2-dev="2.7.15-r0" \
   readline-dev="${READLINE_DEV_PKG_VERSION:-6.3.008-r5}" \
-  sqlite-dev="${SQLITE_DEV_PKG_VERSION:-3.20.1-r1}" \
+  sqlite-dev="${SQLITE_DEV_PKG_VERSION:-3.20.1-r2}" \
   xz-libs="${XZ_LIBS_PKG_VERSION:-5.2.3-r0}" \
   zlib-dev="${ZLIB_DEV_PKG_VERSION:-1.2.11-r0}"
 
